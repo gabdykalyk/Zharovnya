50fcbcdeaed84ec37a8bfcc91de5638a22650e8f
gabdykalyk <gabdykalyk@gmail.com>
2020-08-19 21:35:05 +0600

    count added

--- src/script/main.js
+++ src/script/main.js
@@ -5,6 +5,10 @@ new Vue({
         isActive: false,
         count: 1,
         price: 2300,
+        firstDish: 1,
+        secondDish: 1,
+        thirdDish: 1,
+        fourthDish: 1
     },
     methods: {
         myFilter: function () {
@@ -12,13 +16,37 @@ new Vue({
         },
         increase: function () {
             this.count++
-            this.price = this.price + 2300
+            this.price = 2300 * this.count
         },
         decreas: function () {
             if (this.count >= 2) {
                 this.count--
                 this.price = this.price - 2300
             }
+        },
+        increaseFirst: function () {
+            this.firstDish++
+        },
+        decreasFirst: function () {
+            this.firstDish--
+        },
+        increaseSecond: function () {
+            this.secondDish++
+        },
+        decreasSecond: function () {
+            this.secondDish--
+        },
+        increaseThird: function () {
+            this.thirdDish++
+        },
+        decreasThird: function () {
+            this.thirdDish--
+        },
+        increaseFourth: function () {
+            this.fourthDish++
+        },
+        decreasFourth: function () {
+            this.fourthDish--
         }
     },
     computed: {
